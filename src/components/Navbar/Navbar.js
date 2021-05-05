b5f0c999d64a54cf8acdb667d319518346a7343e
Jhon Steven Saavedra <65989472+jsgsco@users.noreply.github.com>
2021-05-04 22:06:45 -0500

    Carrousel terminado, y agregado al Home, Navbar en proceso

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import './Navbar.sass'
+
+const Navbar = () => {
+    return (
+        <div>
+            desde Navbar
+        </div>
+    )
+}
+
+export default Navbar
--- src/components/pages/Auth/Home/Home.js
+++ src/components/pages/Auth/Home/Home.js
@@ -1,9 +1,15 @@
+import Carrousel from '../../../Carrousel'
+import Navbar from '../../../Navbar/Navbar'
+
 import './Home.sass'
 
 const Home = () => {
     return (
-        <div>
-            desde Home
+        <div className="home">
+            <div className="home_container">
+                <Navbar />
+                <Carrousel />
+            </div>
         </div>
     )
 }
